9e190bb75da812ad8f0153bf679079001364845b
juliakoa <julisia199706@gmail.com>
2023-08-20 15:50:07 +0200

    localstorage added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,19 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = newContact => {
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
